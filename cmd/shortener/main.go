258f9aff5a65c6a1829cb06dd96da6bfaa696a0c
Никитин Дмитрий <sapa408@gmail.com>
2025-11-04 21:52:04 +0700

    добавил сохранение сокращенных url в файл

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
-	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/dsnikitin/shortener/internal/config"
 	"github.com/dsnikitin/shortener/internal/handler"
@@ -12,18 +14,30 @@ import (
 )
 
 func main() {
-	c := config.NewFromArgs()
-	r := repository.NewMemory()
-	s := service.New(r)
-	h := handler.New(c, s)
+	conf := config.NewFromArgs()
 
-	if err := logger.Initialize(c.LogLevel); err != nil {
+	if err := logger.Initialize(conf.LogLevel); err != nil {
 		log.Fatalf("init logger error: %s", err)
 	}
 
-	logger.Log.Sugar().Infow("Running server", "address", c.ServerAddr)
-
-	if err := http.ListenAndServe(c.ServerAddr, newChiMux(h)); err != nil {
-		logger.Log.Sugar().Fatalw("running server", "error", err)
+	file, err := os.OpenFile(conf.FileStoragePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		logger.Log.Sugar().Fatalw("open storage file", "error", err)
 	}
+	defer file.Close()
+
+	repo, _ := repository.New(file)
+	service := service.New(repo)
+	handler := handler.New(conf, service)
+	server := initServer(conf, newChiMux(handler))
+
+	shutdownSignalChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownSignalChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	go start(server)
+
+	<-shutdownSignalChan
+
+	logger.Log.Sugar().Info("received shutdown signal")
+	shutdown(server)
 }
